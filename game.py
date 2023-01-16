92ee307e770a79836e7b2b482ff2c8aa80fcb68e
ice8980 <79262119+ice8980@users.noreply.github.com>
2023-01-16 10:03:04 +0700

    New code

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
-import cmd
+ # Snake Using Pygame   
+  
+import math  
+import random  
+import pygame
+import os  
+import tkinter as tk  
+from tkinter import messagebox  
+import discord  
+  
+class cube(object):  
+    rows = 20  
+    w = 500  
+  
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):  
+        self.pos = start  
+        self.dirnx = 1  
+        self.dirny = 0  
+        self.color = color  
+def space(object):
+    guns = 30
+    lasers = 40
+   
